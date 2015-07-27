cb1f194847e60935c7803384e2c67042ea168df7
ashishkamat <ashishka@cybage.com>
2015-07-27 04:45:17 -0700

    Added sample sql files removed shell script

---
+++ ecomm_project/SQL/migrate/a.sql
@@ -0,0 +1,5 @@
+show tables;
+select * from admin;
+ALTER TABLE admin ADD admin_age int(5);
+select * from admin;
+exit
--- ecomm_project/SQL/migrate/b.sql
+++ ecomm_project/SQL/migrate/b.sql
@@ -1,6 +1,5 @@
-use "ecomm";
 show tables;
 select * from admin;
-ALTER TABLE admin ADD admin_age int(5);
+ALTER TABLE admin ADD admin_address varchar(200);
 select * from admin;
 exit
